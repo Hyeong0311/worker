a2df58b08d0bc377573e1793fa9cb6b2a4a2ec36
Dongeon Lee <duqdjs123@naver.com>
2024-08-06 11:43:05 +0900

    scno

--- src/main/java/org/example/worker/vo/ScheduleVO.java
+++ src/main/java/org/example/worker/vo/ScheduleVO.java
@@ -11,7 +11,7 @@ import java.sql.Timestamp;
 @ToString
 
 public class ScheduleVO {
-    private Integer sno;
+    private Integer scno;
     private String wid;
     private Timestamp in;
     private Timestamp out;
