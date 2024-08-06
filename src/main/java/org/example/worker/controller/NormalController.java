6981148d16d3b29c634676e94171b0da5031527f
Dongeon Lee <duqdjs123@naver.com>
2024-08-06 17:04:55 +0900

    workerDAO into getTotal method edit

---
+++ src/main/java/org/example/worker/controller/NormalController.java
@@ -0,0 +1,12 @@
+package org.example.worker.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import lombok.extern.log4j.Log4j2;
+
+@WebServlet
+@Log4j2
+public class NormalController extends HttpServlet {
+
+
+}
--- src/main/java/org/example/worker/dao/WorkerDAO.java
+++ src/main/java/org/example/worker/dao/WorkerDAO.java
@@ -7,6 +7,8 @@ import org.example.worker.vo.WorkerVO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
 
 
 @Log4j2
@@ -48,6 +50,25 @@ public enum WorkerDAO {
         if(count != 1) {
             throw new Exception();
         }
+    }
+
+    public List<WorkerVO> wListSuper(int page) throws Exception {
+
+    }
+
+    public int getTotal() throws Exception {
+
+        String sql = "select count(wno) from worker where wno>0 and wdelflag = false";
+
+        @Cleanup Connection con = ConnectionUtil.INSTANCE.getDs().getConnection();
+        @Cleanup PreparedStatement ps = con.prepareStatement(sql);
+        @Cleanup ResultSet rs = ps.executeQuery();
+
+        rs.next();
+
+        int total = rs.getInt(1);
+
+        return total;
     }
 
 }
