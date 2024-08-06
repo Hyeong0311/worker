1c60df6014951afe08ee087306a09991e8630b2b
jhyeong <hyeongcode@gmail.com>
2024-08-06 17:06:33 +0900

    --

--- src/main/java/org/example/worker/dao/WorkerDAO.java
+++ src/main/java/org/example/worker/dao/WorkerDAO.java
@@ -7,6 +7,7 @@ import org.example.worker.vo.WorkerVO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.util.List;
 
 
 @Log4j2
@@ -48,6 +49,32 @@ public enum WorkerDAO {
         if(count != 1) {
             throw new Exception();
         }
+    }
+
+    public List<WorkerVO> wListHR() throws Exception {
+
+        String query = """
+                select
+                    tmp.wname, schedule.category, schedule.time,tmp.dept, tmp.wid, schedule.note
+                from
+                    schedule inner join
+                    (
+                        select
+                            worker.wname, worker.wid, supervisor.dept
+                        from worker inner join supervisor
+                        where worker.sid = supervisor.sid
+                    ) tmp
+                where
+                    schedule.wid = tmp.wid
+                    and
+                    tmp.wid > 0
+                order by tmp.dept
+                """;
+
+
+
+
+        return null;
     }
 
 }
